ecbe81314b9740d280cb6f40ef16773eb9b9ea5f
Adriel Cedrick B. Florante <152822897+AdrielFlorante@users.noreply.github.com>
2024-11-14 12:20:04 +0800

    Add files via upload
    Added supabaseClient.js to handle connections to supabase instance, auth.js to sample how to run the functions in supabaseClient on how to login. index.js renders App.js with react, App.js mounts Home.js. You may mount more pages this way by making another .js file, for example: Dashboard.js can be mounted in App.js as another page.
    
    Static .html and .css files go into public

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      {/* Additional components like navigation or other pages go here */}
+    </div>
+  );
+}
+
+export default App;
